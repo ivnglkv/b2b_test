5f72f3dc469a36e454aa953c0d394cd1b929c08e
Golikov Ivan <ivnglkv@eml.cc>
2018-09-21 19:59:55 +0300

    Добавлена модель YTVideo для хранения найденных видео

--- yt_search_api/models.py
+++ yt_search_api/models.py
@@ -10,3 +10,22 @@ class SearchKeyword(models.Model):
 
     def __str__(self):
         return self.word
+
+
+class YTVideo(models.Model):
+    """
+    Модель YTVideo хранит данные о найденных по ключевым словам видео на YouTube
+    """
+    # YouTube позволяет давать видео названия не длиннее 100 символов
+    # https://developers.google.com/youtube/v3/docs/videos#snippet.title
+    title = models.CharField(verbose_name='заголовок', max_length=100)
+    url = models.URLField(verbose_name='URL')
+    date = models.DateField(verbose_name='дата загрузки')
+    key_words = models.ManyToManyField(
+        SearchKeyword,
+        verbose_name='ключевые слова',
+        related_name='videos',
+    )
+
+    def __str__(self):
+        return '{} ({})'.format(self.title, self.url)
