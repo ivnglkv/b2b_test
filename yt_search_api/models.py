2214a494caf3257f790fffa83778d1427a0c7ca5
Golikov Ivan <ivnglkv@eml.cc>
2018-09-21 23:35:18 +0300

    У поля SearchKeyword.word снято свойство editable=False
    Это не позволяло вносить данные в это поле через REST-отображения

--- yt_search_api/models.py
+++ yt_search_api/models.py
@@ -6,7 +6,7 @@ class SearchKeyword(models.Model):
     Модель SearchKeyword предназначена для хранения ключевых слов, по которым приложение
     производит периодический поиск в YouTube
     """
-    word = models.CharField(verbose_name='слово', max_length=255, unique=True, editable=False)
+    word = models.CharField(verbose_name='слово', max_length=255, unique=True)
 
     def __str__(self):
         return self.word
