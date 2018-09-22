0cfa1378d66d3d2a3cbca659d0661c94ab556c7e
Golikov Ivan <ivnglkv@eml.cc>
2018-09-22 11:39:17 +0300

    Возврат 404 при запросе видео по несуществующему слову

--- yt_search_api/views.py
+++ yt_search_api/views.py
@@ -1,4 +1,7 @@
+from django.core.exceptions import ObjectDoesNotExist
+
 from rest_framework import generics
+from rest_framework.exceptions import NotFound
 
 from .models import SearchKeyword, YTVideo
 from .pagination import MediumResultSetPagination
@@ -21,6 +24,14 @@ class YTVideoList(generics.ListAPIView):
 
     def get_queryset(self):
         keyword_pk = self.kwargs['keyword_pk']
+
+        # В случае, если ключевого слова с таким id не существует, вернуть 404
+        # Иначе будет не отличить ответы в случаях, если к ключевому слову не найдены видео
+        # и когда запрошенного ключевого слова не существует
+        try:
+            SearchKeyword.objects.get(pk=keyword_pk)
+        except ObjectDoesNotExist:
+            raise NotFound('Keyword not found')
 
         queryset = YTVideo.objects.filter(key_words__pk=keyword_pk)
 
