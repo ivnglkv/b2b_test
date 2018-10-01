1e7c09669420aee5d6f31856b000b937aab95b3f
Golikov Ivan <ivnglkv@eml.cc>
2018-10-01 18:10:53 +0300

    В админку добавлены YTVideo и SearchKeyword

--- yt_search_api/admin.py
+++ yt_search_api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import SearchKeyword, YTVideo
+
+
+class YTVideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url')
+
+
+admin.site.register(SearchKeyword)
+admin.site.register(YTVideo, YTVideoAdmin)
